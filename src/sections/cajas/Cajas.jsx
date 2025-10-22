632c21f9b2cc192fcb896859682145d82cf367cd
Candela94 <candelafsg@alumnos.cei.es>
2025-10-22 11:37:52 +0200

    pruebas

--- src/sections/cajas/Cajas.jsx
+++ src/sections/cajas/Cajas.jsx
@@ -18,55 +18,55 @@ const CajasSection = () => {
     const [cajaAbierta, setCajaAbierta] = useState(null);
 
     const handleToggle = (id) => {
-      setCajaAbierta(prev => prev === id ? null : id); // si clicas la misma, la cierra
+        setCajaAbierta(prev => prev === id ? null : id); // si clicas la misma, la cierra
     };
 
-    return ( 
+    return (
 
 
 
         <>
-        <section className="section section-cajas">
+            <section className="section section-cajas">
 
 
-        <h1 className='titulo-cajas'>ELIGE <br /> LO JUSTO PARA <br /> UN GRAN MOMENTO</h1>
+                <h1 className='titulo-cajas'>ELIGE <br /> LO JUSTO PARA <br /> UN GRAN MOMENTO</h1>
 
 
-<p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum voluptate dolor! Dolore eligendi, debitis sunt eos nam error perspiciatis fugit labore, maxime nobis inventore commodi necessitatibus. Hic, amet nisi.
-Doloribus consectetur voluptatem officiis dignissimos illo facilis natus labore at dolor assumenda sit, eaque consequatur illum dicta sint eligendi quo, non nostrum nisi aliquid praesentium. Laborum unde minima aspernatur voluptatum?</p>
-
-     
-
-
-        <div className="galeria">
-            
-       
-            {
-
-                cajas.map((c,id) => (
-                    <ul className="galeria-cajas">
-                        <li key={id} className="cajas-li"><Cajas caja={c} isOpen={cajaAbierta===id}          onToggle={() => handleToggle(id)}
-                        /></li>
-
-                    </ul>
-                ))
-            }
-        </div>
+                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum voluptate dolor! Dolore eligendi, debitis sunt eos nam error perspiciatis fugit labore, maxime nobis inventore commodi necessitatibus. Hic, amet nisi.
+                    Doloribus consectetur voluptatem officiis dignissimos illo facilis natus labore at dolor assumenda sit, eaque consequatur illum dicta sint eligendi quo, non nostrum nisi aliquid praesentium. Laborum unde minima aspernatur voluptatum?</p>
 
 
 
-        {/* <Contacto /> */}
+
+                <div className="galeria">
 
 
-        </section>
-        
-        
+                    {
+
+                        cajas.map((c, id) => (
+                            <ul className="galeria-cajas">
+                                <li key={id} className="cajas-li"><Cajas caja={c} isOpen={cajaAbierta === id} onToggle={() => handleToggle(id)}
+                                /></li>
+
+                            </ul>
+                        ))
+                    }
+                </div>
+
+
+
+                {/* <Contacto /> */}
+
+
+            </section>
+
+
         </>
-     );
+    );
 }
 
 
 
 
- 
+
 export default CajasSection;
