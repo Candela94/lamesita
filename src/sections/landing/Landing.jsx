49d8f993a609d5c9c669834a8b9a04635fc59859
Candela94 <candelafsg@alumnos.cei.es>
2025-11-05 20:31:07 +0100

    .

--- src/sections/landing/Landing.jsx
+++ src/sections/landing/Landing.jsx
@@ -1,41 +1,50 @@
+
+
 import './landing.css'
 import '../../css/App.css'
 import Introduccion from '../intro/Introduccion';
+
 import Info from '../info/Info';
 import About from '../about/About';
 import CajasSection from '../cajas/Cajas';
 import Galeria from '../../component/galeria/Galeria';
+
 import {Seccion} from '../../component/seccion/Seccion';
 import { Header, HeaderDesk } from '../../component/header/Header';
 import Datos from '../datos/Datos';
 
+
 const Landing = () => {
+
     return (
+
+
         <>
-            <Header className='h-mobile'/>
-            <HeaderDesk className='h-desk'/>
-            
-            <main className='section main-principal'>
-                <Seccion id='intro'>
-                    <Introduccion />
-                </Seccion>
+       
 
-                <Seccion id='about'>
-                    <About />
-                </Seccion>
+       
+        <Header className='h-mobile'/>
+        <HeaderDesk className='h-desk'/>
 
-                <Seccion id='galeria'>
-                    <Galeria />
-                </Seccion>
-                
-                <Seccion id='cajas'>
-                    <CajasSection />
-                </Seccion>
+        
+        <main className='section main-principal'>
+         <Introduccion id='intro' />
+           <About id='about'/>
+           <Galeria  id='galeria'/>
+         
+           <CajasSection id='cajas'/>
+           <Info id='info'/> 
 
-                <Seccion id='info'>
-                    <Info />
-                </Seccion>
-            </main>
+
+
+
+
+
+
+
+           
+        </main>
+        
         </>
     );
 }
